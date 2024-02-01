ad75496cc26cb6da8bb4e8ae25ff0c7936ea7dd3
krishna472004 <morthikrishna2017@gmail.com>
2024-02-02 00:46:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mini3/home.java
@@ -0,0 +1,28 @@
+package com.example.mini3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mini3.R;
+
+public class home extends AppCompatActivity {
+    public void second(View V) {
+        Intent i = new Intent(this,homepage.class);
+        startActivity(i);
+    }
+    public void third(View V) {
+        Intent i = new Intent(this,clublist.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Add any additional initialization or logic for the Home activity here
+    }
+}
